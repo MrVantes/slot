71097fd98001ba535f6ab46b1b8425e709b2fc03
MrVantes <adib.maimon@gmail.com>
2024-12-11 15:15:43 +0700

    feat: add getUser and getBet func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MrVantes/slot
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func getName() string {
+	name := ""
+	fmt.Println("Slot")
+	fmt.Printf("Enter your name:")
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		return ""
+	}
+	fmt.Printf("Welcome %s, let's play!\n", name)
+	return name
+}
+
+func getBet(balance uint) uint {
+	var bet uint
+	for true {
+		fmt.Printf("Enter your bet, or 0 to quit (balance = $%d:)", balance)
+		fmt.Scan(&bet)
+
+		if bet > balance {
+			fmt.Println("Bet cannot be larger than balance")
+		} else {
+			break
+		}
+	}
+	return bet
+}
+
+func main() {
+	balance := 200
+	getName()
+
+}
