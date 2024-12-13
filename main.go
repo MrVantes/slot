5d59dd95bb90b28c26631571c3e25221ccfebf2f
MrVantes <adib.maimon@gmail.com>
2024-12-13 14:34:30 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func getName() string {
 	name := ""
@@ -16,7 +19,7 @@ func getName() string {
 
 func getBet(balance uint) uint {
 	var bet uint
-	for true {
+	for {
 		fmt.Printf("Enter your bet, or 0 to quit (balance = $%d:)", balance)
 		fmt.Scan(&bet)
 
@@ -29,8 +32,72 @@ func getBet(balance uint) uint {
 	return bet
 }
 
+func generateSymbolArray(symbols map[string]uint) []string {
+	symbolArr := []string{}
+	for symbol, count := range symbols {
+		for i := uint(0); i < count; i++ {
+			symbolArr = append(symbolArr, symbol)
+		}
+	}
+	return symbolArr
+}
+
+func getRandomNumber(min int, max int) int {
+	randomNumber := rand.Intn(max-min+1) + min
+	return randomNumber
+}
+
+func getSpin(reel []string, rows int, cols int) [][]string {
+	result := [][]string{}
+
+	for i := 0; i < rows; i++ {
+		result = append(result, []string{})
+	}
+
+	for col := 0; col < cols; col++ {
+		selected := map[int]bool{}
+		for row := 0; row < rows; row++ {
+			for {
+				randomIndex := getRandomNumber(0, len(reel)-1)
+				_, exists := selected[randomIndex]
+				if !exists {
+					selected[randomIndex] = true
+					result[row] = append(result[row], reel[randomIndex])
+					break
+				}
+			}
+		}
+	}
+	return result
+}
+
 func main() {
-	balance := 200
+	symbols := map[string]uint{
+		"A": 4,
+		"B": 7,
+		"C": 12,
+		"D": 20,
+	}
+	// multipler := map[string]uint{
+	// 	"A": 20,
+	// 	"B": 10,
+	// 	"C": 5,
+	// 	"D": 2,
+	// }
+
+	symbolArr := generateSymbolArray(symbols)
+	spin := getSpin(symbolArr, 3, 3)
+	fmt.Println(spin)
+
+	balance := uint(200)
 	getName()
 
+	for balance > 0 {
+		bet := getBet(balance)
+		if bet == 0 {
+			break
+		}
+		balance -= bet
+	}
+	fmt.Printf("You are left with: $%d.\n", balance)
 }
